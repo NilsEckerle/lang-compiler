2373505dcada0abb6c4e9fcd5f3143db49547a76
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-11-25 13:33:03 +0100

    feat(cli): add argparse for multi-file support

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
+#include "argparse/argparse.hpp"
 #include "lexer/lexer.h"
 #include "tokens.h"
+#include <cstdlib>
 #include <iostream>
+#include <ostream>
+#include <string>
 #include <vector>
 
-int main() {
-  std::vector<lexer::token_t *> tokens =
-      lexer::lex_file("examples/expressions.lang");
+int main(int argc, char *argv[]) {
+  // setup argparse
+  argparse::ArgumentParser program("lang");
 
-  for (lexer::token_t *tok : tokens) {
-    lexer::debug_print(tok);
+  program.add_argument("source_files")
+      .help("source files to compile")
+      .remaining();
+
+  // argparse parse the arguments
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return 1;
   }
-  std::cout << std::endl;
+
+  // Check if no source files provided
+  if (!program.is_used("source_files")) {
+    std::cout << program;
+    return 1;
+  }
+
+  // read arguments
+  std::vector<std::string> source_files =
+      program.get<std::vector<std::string>>("source_files");
+
+  // start lexing
+  for (std::string source_path : source_files) {
+    std::vector<lexer::token_t *> tokens = lexer::lex_file(source_path);
+
+    std::cout << source_path << std::endl;
+    for (lexer::token_t *tok : tokens) {
+      lexer::debug_print(tok);
+    }
+    std::cout << std::endl;
+  }
 
   return 0;
 }
