9acae23e42d9f26fcc1f168570e901ad5a8e1e99
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-12-01 13:31:31 +0100

    feat(main): integrate parser and logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "argparse/argparse.hpp"
 #include "lexer/lexer.h"
+#include "parser/parser.h"
+#include "parser/syntax_tree.h"
+#include "spdlog/spdlog.h"
 #include "tokens.h"
 #include <cstdlib>
 #include <iostream>
@@ -7,43 +10,69 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char *argv[]) {
-  // setup argparse
-  argparse::ArgumentParser program("lang");
+void setup_spdlog() { spdlog::set_level(spdlog::level::debug); }
 
-  program.add_argument("source_files")
+void setup_argpars(int argc, char *argv[], argparse::ArgumentParser *program) {
+  // setup argparse
+  (*program)
+      .add_argument("source_files")
       .help("source files to compile")
       .remaining();
 
   // argparse parse the arguments
   try {
-    program.parse_args(argc, argv);
+    (*program).parse_args(argc, argv);
   } catch (const std::exception &err) {
-    std::cerr << err.what() << std::endl;
-    std::cerr << program;
-    return 1;
+    spdlog::critical("Error parsing arguments. Terminating...:\n\t{0}",
+                     err.what());
+    exit(1);
   }
 
   // Check if no source files provided
-  if (!program.is_used("source_files")) {
-    std::cout << program;
-    return 1;
+  if (!(*program).is_used("source_files")) {
+    spdlog::info("No source files are provided. Terminating...");
+    exit(1);
   }
+}
 
-  // read arguments
-  std::vector<std::string> source_files =
-      program.get<std::vector<std::string>>("source_files");
-
-  // start lexing
+void run_compiler(std::vector<std::string> source_files) {
+  // start compiling
   for (std::string source_path : source_files) {
+    // tokenize
     std::vector<lexer::token_t *> tokens = lexer::lex_file(source_path);
+    spdlog::info("Lexing finished");
+    // parse
+    parser::syntax_tree_t *tree = parser::parse_tokens(tokens);
+    spdlog::info("Parsing finished");
+    // Code generation finished
 
+    spdlog::info("Code generation finished");
+
+    //
+    // DEBUG PRINTING
+    //
+    // debug print tokens
     std::cout << source_path << std::endl;
     for (lexer::token_t *tok : tokens) {
       lexer::debug_print(tok);
     }
     std::cout << std::endl;
+
+    // debug print syntax_tree
+    std::cout << tree->to_prefix_notation() << std::endl;
   }
+}
+
+int main(int argc, char *argv[]) {
+  setup_spdlog();
+  argparse::ArgumentParser program = argparse::ArgumentParser("lang");
+  setup_argpars(argc, argv, &program);
+
+  // read arguments
+  std::vector<std::string> source_files =
+      program.get<std::vector<std::string>>("source_files");
+
+  run_compiler(source_files);
 
   return 0;
 }
