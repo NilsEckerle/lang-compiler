095f489532e615278e1ac3a487f837ad27d3a110
Nils Eckerle <nilseckerle2001@protonmail.com>
2026-01-26 11:21:11 +0100

    docs(parser): add doxygen comments to parser interface

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -1,3 +1,11 @@
+/**
+ * @file parser.h
+ * @brief Main parser interface for converting tokens to AST
+ *
+ * This file defines the parser interface for transforming a stream of tokens
+ * into an abstract syntax tree representation.
+ */
+
 #ifndef PARSER_H
 #define PARSER_H
 
@@ -8,8 +16,22 @@
 namespace compiler {
 namespace parser {
 
+/**
+ * @class parser_t
+ * @brief Base parser class
+ *
+ * Placeholder for future parser functionality.
+ */
 class parser_t {};
 
+/**
+ * @brief Parses a vector of tokens into abstract syntax trees
+ * @param tokens Vector of token pointers to parse
+ * @return Vector of AST pointers representing the parsed program
+ *
+ * Processes the token stream and constructs ASTs for all top-level
+ * declarations and definitions in the source code.
+ */
 std::vector<ast::abstract_syntax_tree_t *> *
 parse_tokens(std::vector<token_t *> &tokens);
 
